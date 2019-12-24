1670982d01a8caa2d1af7c55e6ca9c9d19424cf8
Jacob Hill <Jacob.Dylan.Hill@gmail.com>
2019-12-24 14:00:57 -0500

    Add project files.

---
+++ JacobHillResumeWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JacobHillResumeWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
